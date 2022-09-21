cc88430fdaa2534572768a93403a9b9e9329ae3c
Ifka Nabilah <85624063+zoroaster-dot@users.noreply.github.com>
2022-09-22 04:49:00 +0700

    Create beranda.js

---
+++ src/Components/beranda.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+class Beranda extends React.Component{
+    render(){
+        return(
+            <div className="jumbotron" >
+                <div className="container-fluid">
+                    <h2 class="display-4">Selamat Datang</h2>
+                    <hr></hr>
+                    <p className="lead">Selamat datang ditampilan awal Beranda saya jika ingin tau lebih lanjut 
+                    tentang saya, klik tentang saya pada navbar diatas.</p>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default Beranda;
